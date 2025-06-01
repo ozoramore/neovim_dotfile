c2ead65ebcdd8ca186d3d4990d6b05d91416a9f0
ozoramore <dev@t2arc.net>
2025-06-01 13:24:22 +0900

    plugin.miniの修正

--- lua/plugin/mini.lua
+++ lua/plugin/mini.lua
@@ -1,15 +1,29 @@
-local M = {
-	deps = {
-		add = require('mini.deps').add,
-		now = require('mini.deps').now,
-		path_package = vim.fn.stdpath('data') .. '/site/'
-	},
-	statusline = {
-		contents = {
-			content = {
-				active = require('plugin.styles').statusline,
-				inactive = require('plugin.styles').statusline,
-			}
+local M = {}
+
+M.deps = {
+	add = require('mini.deps').add,
+	now = require('mini.deps').now,
+	path_package = vim.fn.stdpath('data') .. '/site/'
+}
+
+local function statusline()
+	local mode, mode_hl = require('mini.statusline').section_mode({})
+	local filename      = require('mini.statusline').section_filename({})
+	local separator     = '%='
+	local fileinfo      = require('mini.statusline').section_fileinfo({})
+	local location      = '%4l:%3c'
+
+	return require('mini.statusline').combine_groups({
+		{ hl = mode_hl,      strings = { mode:upper() } },
+		{ hl = 'StatusLine', strings = { filename, separator, fileinfo, location } },
+	})
+end
+
+M.statusline = {
+	contents = {
+		content = {
+			active = statusline,
+			inactive = statusline,
 		}
 	}
 }
@@ -30,13 +44,16 @@ M.packadd = function()
 end
 
 M.setup = function()
-	M.deps.now(function() M.deps.add({ name = 'mini.nvim' }) end)
-	require('mini.deps').setup({ path = { package = M.deps.path_package } })
-	require('mini.completion').setup()
-	require('mini.snippets').setup()
-	require('mini.tabline').setup()
-	require('mini.statusline').setup(M.statusline.contents)
-	vim.api.nvim_create_autocmd('User', { pattern = 'MiniSnippetsSessionStart', callback = snippets_stop })
+	local setup = function()
+		M.deps.add({ name = 'mini.nvim' })
+		require('mini.deps').setup({ path = { package = M.deps.path_package } })
+		require('mini.completion').setup()
+		require('mini.snippets').setup()
+		require('mini.tabline').setup()
+		require('mini.statusline').setup(M.statusline.contents)
+		vim.api.nvim_create_autocmd('User', { pattern = 'MiniSnippetsSessionStart', callback = snippets_stop })
+	end
+	M.deps.now(setup)
 end
 
 return M
--- lua/plugin/styles.lua
+++ lua/plugin/styles.lua
@@ -1,16 +1,3 @@
 local M = {}
 
-M.statusline = function()
-	local mode, mode_hl = require('mini.statusline').section_mode({})
-	local filename      = require('mini.statusline').section_filename({})
-	local separator     = '%='
-	local fileinfo      = require('mini.statusline').section_fileinfo({})
-	local location      = '%4l:%3c'
-
-	return require('mini.statusline').combine_groups({
-		{ hl = mode_hl,      strings = { mode:upper() } },
-		{ hl = 'StatusLine', strings = { filename, separator, fileinfo, location } },
-	})
-end
-
 return M
