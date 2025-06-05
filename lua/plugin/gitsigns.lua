60f39f168a37bff2e31cd8c76c15a89d7f3fd89f
ozoramore <dev@t2arc.net>
2025-06-05 17:03:55 +0900

    gitsignsの修正

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -1,14 +1,15 @@
 local M = {}
 
+local git_signs = {
+	add          = { text = '║' },
+	change       = { text = '┃' },
+	delete       = { text = '⎣' },
+	topdelete    = { text = '⎡' },
+	changedelete = { text = '╪' },
+	untracked    = { text = '┆' },
+}
+
 function M.setup()
-	local git_signs = {
-		add          = { text = '║' },
-		change       = { text = '┃' },
-		delete       = { text = '⎣' },
-		topdelete    = { text = '⎡' },
-		changedelete = { text = '╪' },
-		untracked    = { text = '┆' },
-	}
 	require('gitsigns').setup({ signs = git_signs, signs_staged = git_signs })
 end
 
