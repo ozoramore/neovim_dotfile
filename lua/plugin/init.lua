957a03c30e1a715ff74a2835b15bd2d9758c3735
ozoramore <dev@t2arc.net>
2025-06-05 16:06:15 +0900

    plugin.luaの整理

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,30 +1,28 @@
-local M = {}
+local PLUGIN = {}
 
-local load = function(src, setup)
-	local load = function()
+PLUGIN.load = function(src, setup)
+	local _load = function()
 		if src then require('plugin.mini').deps.add({ source = src }) end
 		if setup then setup() end
 	end
-	require('plugin.mini').deps.now(load)
+	require('plugin.mini').deps.now(_load)
 end
 
-M.load = load
-
-M.setup = function() -- plugin設定
+PLUGIN.setup = function() -- plugin設定
 	require('plugin.mini').setup()
 	require('plugin.fep').setup()
 
-	load('folke/styler.nvim', require('plugin.styler').setup)
-	load('lewis6991/gitsigns.nvim', require('plugin.gitsigns').setup)
-	load('luukvbaal/statuscol.nvim', require('plugin.statuscol').setup)
+	PLUGIN.load('folke/styler.nvim', require('plugin.styler').setup)
+	PLUGIN.load('lewis6991/gitsigns.nvim', require('plugin.gitsigns').setup)
+	PLUGIN.load('luukvbaal/statuscol.nvim', require('plugin.statuscol').setup)
 
-	load('ozoramore/nvimpc.lua', require('plugin.mpc').setup)
+	PLUGIN.load('ozoramore/nvimpc.lua', require('plugin.mpc').setup)
 
 	if vim.fn.has('unix') == 1 then
-		load('mfussenegger/nvim-dap', require('plugin.dap').setup)
-		load('nvim-treesitter/nvim-treesitter', require('plugin.treesitter').setup)
-		load('neovim/nvim-lspconfig', require('plugin.lsp').setup)
+		PLUGIN.load('mfussenegger/nvim-dap', require('plugin.dap').setup)
+		PLUGIN.load('nvim-treesitter/nvim-treesitter', require('plugin.treesitter').setup)
+		PLUGIN.load('neovim/nvim-lspconfig', require('plugin.lsp').setup)
 	end
 end
 
-return M
+return PLUGIN
