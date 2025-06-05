19942481b338ceeb2efc4a15d1b9e3945cd391ba
ozoramore <dev@t2arc.net>
2025-06-05 16:23:42 +0900

    statuscolの整理

--- lua/plugin/statuscol.lua
+++ lua/plugin/statuscol.lua
@@ -1,21 +1,21 @@
-local M = {}
+local STATUSCOL = {}
 
-function M.setup()
-	local function signs(ns, add_sign)
-		local sign = { namespace = { ns }, maxwidth = 1, colwidth = 1, auto = false }
-		for k, v in pairs(add_sign) do sign[k] = v end
-		return { sign = sign, click = 'v:lua.ScSa' }
-	end
-	require('statuscol').setup({
-		bt_ignore = { 'terminal', 'nofile' },
-		segments = {
-			signs('diagnostic/signs', { serverity_sort = true }),
-			signs('Dap', {}),
-			{ text = { '%l' },                                  click = 'v:lua.ScLa' },
-			{ text = { require('statuscol.builtin').foldfunc }, click = 'v:lua.ScFa' },
-			signs('git', { wrap = true, fillchar = '│', fillcharhl = 'NonText' }),
-		}
-	})
+local function signs(ns, add_sign)
+	local sign = { namespace = { ns }, maxwidth = 1, colwidth = 1, auto = false }
+	for k, v in pairs(add_sign) do sign[k] = v end
+	return { sign = sign, click = 'v:lua.ScSa' }
 end
 
-return M
+function STATUSCOL.setup()
+	local builtin = require('statuscol.builtin')
+	local ignore = { 'terminal', 'nofile' }
+	local diag = signs('diagnostic', { serverity_sort = true })
+	local dap = signs('Dap', {})
+	local lnum = { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' }
+	local fold = { text = { builtin.foldfunc }, click = 'v:lua.ScFa' }
+	local git = signs('git', { wrap = true, fillchar = '│', fillcharhl = 'NonText' })
+
+	require('statuscol').setup({ bt_ignore = ignore, segments = { diag, dap, lnum, fold, git } })
+end
+
+return STATUSCOL
