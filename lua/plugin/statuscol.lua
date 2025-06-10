39d0e9026c7e852e2f32b2759ccb37f784ba1a3e
ozoramore <dev@t2arc.net>
2025-06-10 15:54:51 +0900

    gitsigns/dapのStatuscolを調整

--- lua/plugin/statuscol.lua
+++ lua/plugin/statuscol.lua
@@ -1,7 +1,8 @@
 local STATUSCOL = {}
 
 local function signs(ns, add_sign)
-	local sign = { namespace = { ns }, maxwidth = 1, colwidth = 1, auto = false }
+	local sign = { maxwidth = 1, colwidth = 1, auto = false }
+	if ns then sign.namespace = { ns } end
 	for k, v in pairs(add_sign) do sign[k] = v end
 	return { sign = sign, click = 'v:lua.ScSa' }
 end
@@ -10,10 +11,10 @@ function STATUSCOL.setup()
 	local builtin = require('statuscol.builtin')
 	local ignore = { 'terminal', 'nofile', 'help' }
 	local diag = signs('diagnostic', { serverity_sort = true })
-	local dap = signs('Dap', {})
+	local dap = signs(nil, { name = { 'Dap.*' } })
 	local lnum = { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' }
 	local fold = { text = { builtin.foldfunc }, click = 'v:lua.ScFa' }
-	local git = signs('git', { wrap = true, fillchar = '│', fillcharhl = 'NonText' })
+	local git = signs('gitsigns.*', { wrap = true, fillchar = '│', fillcharhl = 'NonText' })
 
 	require('statuscol').setup({ bt_ignore = ignore, segments = { diag, dap, lnum, fold, git } })
 end
