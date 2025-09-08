e059d6d770287a295553f2c68f63f028ee8104c7
ozoramore <dev@t2arc.net>
2025-09-08 11:03:49 +0900

    mpc.luaの修正
    * MPD_HOSTがない環境では無効化

--- lua/plugin/mpc.lua
+++ lua/plugin/mpc.lua
@@ -1,34 +1,13 @@
 local M = {}
-local lead_filter = function(a)
-	return function(i)
-		return vim.startswith(i, a) or vim.startswith(i, '"' .. a)
-	end
-end
-
-local split = function(str, ts)
-	local t = {}
-	local ptn = '([^' .. (ts or '\n') .. ']+)'
-	for s in string.gmatch(str, ptn) do table.insert(t, s) end
-	return t
-end
 
 M.setup = function()
+	local conf = {  host = os.getenv('MPD_HOST'), port = os.getenv('MPD_PORT') }
+	if conf.host == nil then return end
 	local mpc = require('nvimpc')
-	mpc.setup({ host = os.getenv('MPD_HOST'), port = os.getenv('MPD_PORT') })
-	local cmp_command = function(a, _, _) return vim.tbl_filter(lead_filter(a), mpc.commands) end
-	local cmp_files = function(a, _, _) return vim.tbl_filter(lead_filter(a), mpc.files) end
-
-	local cmp = function(a, l, p)
-		local t = split(l, ' ')
-		if #t < 2 or (#t == 2 and a ~= "") then return cmp_command(a, l, p) end
-		if t[2] == 'add' then return cmp_files(a, l, p) end
-		if t[2] == 'listfiles' then return cmp_files(a, l, p) end
-		return nil
-	end
-
-	vim.api.nvim_create_user_command('Mpc', mpc.print, { nargs = '*', complete = cmp })
-	vim.api.nvim_create_user_command('MpcNowPlaying', mpc.nowplaying, {})
-	vim.api.nvim_create_user_command('MpcQueue', mpc.queue, {})
+	mpc.setup(conf)
+	local mpc_util = require('nvimpc.util')
+	vim.api.nvim_create_user_command('MpcNowPlaying', mpc.gen('currentsong', mpc_util.printer), {})
+	vim.api.nvim_create_user_command('MpcQueue', mpc.gen('playlistinfo', mpc_util.printer), {})
 end
 
 return M
