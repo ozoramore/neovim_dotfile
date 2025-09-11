f9a9d053f82e491ed3b51b24766b656e6f8886d6
ozoramore <dev@t2arc.net>
2025-09-11 20:55:54 +0900

    mpc.luaのアップデート

--- lua/plugin/mpc.lua
+++ lua/plugin/mpc.lua
@@ -1,13 +1,22 @@
 local M = {}
 
+local displaySong = function(tbl) return string.format('%2d: %s / %s - %s', tbl.Track, tbl.Title, tbl.Artist, tbl.Album) end
+
 M.setup = function()
-	local conf = {  host = os.getenv('MPD_HOST'), port = os.getenv('MPD_PORT') }
+	local conf = { host = os.getenv('MPD_HOST'), port = os.getenv('MPD_PORT') }
 	if conf.host == nil then return end
 	local mpc = require('nvimpc')
 	mpc.setup(conf)
-	local mpc_util = require('nvimpc.util')
-	vim.api.nvim_create_user_command('MpcNowPlaying', mpc.gen('currentsong', mpc_util.printer), {})
-	vim.api.nvim_create_user_command('MpcQueue', mpc.gen('playlistinfo', mpc_util.printer), {})
+
+	local printsongs = function(result)
+		local mpc_util = require('nvimpc.util')
+		local t = {}
+		for _, v in mpc_util.devide(result) do table.insert(t, displaySong(v)) end
+		mpc_util.printer(t)
+	end
+
+	vim.api.nvim_create_user_command('MpcNowPlaying', mpc.gen('currentsong', printsongs), {})
+	vim.api.nvim_create_user_command('MpcQueue', mpc.gen('playlistinfo', printsongs), {})
 end
 
 return M
