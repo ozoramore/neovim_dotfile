628abd53d0859080e8aa2bb0358127164959d3c0
ozoramore <dev@t2arc.net>
2025-10-07 10:04:38 +0900

    行番号ハイライトの修正

--- lua/plugin/lnum_color.lua
+++ lua/plugin/lnum_color.lua
@@ -2,9 +2,8 @@ local lnumfunc = function(lnum, current_line)
 	local range = math.abs(lnum - current_line)
 	if range == 0 then return nil end
 	if range == 5 then return 'LineNrGroup1' end
-	if range == 10 then return 'LineNrGroup2' end
-	if range == 20 then return 'LineNrGroup2' end
 	if range % 50 == 0 then return 'LineNrGroup3' end
+	if range % 10 == 0 then return 'LineNrGroup2' end
 	return nil
 end
 
