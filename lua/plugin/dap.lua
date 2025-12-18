c8b23dbde2e56c57977d59501b7a780935700705
ozoramore <dev@t2arc.net>
2025-12-18 13:14:44 +0900

    dapの対応

--- lua/plugin/dap.lua
+++ lua/plugin/dap.lua
@@ -6,23 +6,18 @@ local gdb = {
 	args = { '-i=dap', '-q' },
 }
 
-local get_launch_json = function()
-	local buf = vim.api.nvim_buf_get_name(0)
-	local root = require('util.file').root_dir(buf)
-	if not root then return nil end
+local get_launch_json = function(path)
+	local root = require('util.file').root_dir(path) or '.'
 	return root .. '/.vscode/launch.json'
 end
 
-local get_debugger = function()
-	local filetype = vim.bo.filetype
-	local json = get_launch_json()
-	if not json then return end
-	local config = require('dap.ext.vscode').getconfigs(json)
-	require('dap').configurations[filetype] = config
-end
-
-local setup_dap_view = function()
-	require('dap-view').setup()
+local get_debugger = function(bufnr)
+	local filetype = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
+	local path = vim.api.nvim_buf_get_name(bufnr)
+	if filetype == '' or path == '' then return {} end
+	local json = get_launch_json(path)
+	if not require('util.file').is_exist(json) then return {} end
+	return require('dap.ext.vscode').getconfigs(json)
 end
 
 local M = {}
@@ -31,8 +26,7 @@ M.setup = function()
 	local dap = require('dap')
 
 	dap.adapters.gdb = gdb
-
-	vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { callback = get_debugger })
+	dap.providers.configs['dap.launch.json'] = get_debugger
 
 	-- keymaps
 	vim.keymap.set({ 'n' }, '<F5>', dap.continue)
@@ -43,10 +37,7 @@ M.setup = function()
 	vim.keymap.set({ 'n' }, '<Leader>B', dap.set_breakpoint)
 
 	--tui
-	setup_dap_view()
-
-	--現在のバッファのデバッガを取得して終了(遅延実行の対応)
-	get_debugger()
+	require('dap-view').setup()
 end
 
 return M
