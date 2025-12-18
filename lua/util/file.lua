ddc920451d1253e0832592a7805065e86662b547
ozoramore <dev@t2arc.net>
2025-12-18 12:47:10 +0900

    file.is_existを用意

--- lua/util/file.lua
+++ lua/util/file.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+M.is_exist = function(p)
+	local f = io.open(p,"r")
+	if not f then return false end
+	f:close()
+	return true
+end
+
 local root_patterns = { ".git", ".clang-format", ".vscode" }
 M.root_dir = function(p)
 	local find_result = vim.fs.find(root_patterns, { upward = true, type = 'directory', path = p })
