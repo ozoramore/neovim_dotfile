a615dfef684b7563b99f8007ce793273c0ddb1d8
ozoramore <dev@t2arc.net>
2026-03-18 16:00:27 +0900

    util.filetypeにbuf optsの設定を追加

--- lua/util/filetype.lua
+++ lua/util/filetype.lua
@@ -10,11 +10,31 @@ local set_ext = function(args)
 	return ext
 end
 
+local set_buf_opts = function(conf)
+	require('util.option').setbufopts(conf)
+end
+
+local default_bufopts = {
+	tabstop = 4,
+	expandtab = true,
+	shiftwidth = 0,
+	softtabstop = -1,
+}
+
 local FILETYPE = {}
 
 FILETYPE.setup = function(config)
 	config = config or {}
 	vim.filetype.add({ extension = set_ext(config) })
+end
+
+FILETYPE.config = function(o)
+	local default = vim.tbl_deep_extend('force', default_bufopts, o.default)
+
+	local defaults = function() return default end
+	local callback = function(t) set_buf_opts(setmetatable(o.config, { __index = defaults })[t.match]) end
+
+	vim.api.nvim_create_autocmd('FileType', { callback = callback })
 end
 
 return FILETYPE
