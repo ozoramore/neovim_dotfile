f326e29413bfcf457c056e5e10a85e4aafa62177
ozoramore <dev@t2arc.net>
2026-04-06 10:01:37 +0900

    mini.depsの初回起動を再調整

--- lua/plugin/mini_deps.lua
+++ lua/plugin/mini_deps.lua
@@ -20,9 +20,10 @@ end
 M.initialize = function(url)
 	if not url then os.exit() end
 
-	vim.system({ 'git', 'clone', '--filter=blob:none', url, mini_path })
+	print('Installing package manager. Please wait a moment..')
+	vim.system({ 'git', 'clone', '--filter=blob:none', url, mini_path }, { text = true }):wait()
 	vim.cmd.helptags('ALL')
-	print('Please restart neovim.')
+	print('Done.')
 
 	if (vim.version.ge(vim.version(), { 0, 12, 0 })) then
 		vim.cmd.restart()
@@ -32,7 +33,7 @@ M.initialize = function(url)
 end
 
 M.setup = function(url)
-	M.has = vim.uv.fs_stat(mini_path)
+	M.has = vim.uv.fs_stat(mini_path .. '/.git')
 	if not M.has then M.initialize(url) end
 
 	require('mini.deps').setup({ path = { package = path_package } })
