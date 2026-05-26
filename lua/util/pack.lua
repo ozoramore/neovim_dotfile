40a5681b379648034cc4699ef2ede9274a6c2b58
ozoramore <dev@t2arc.net>
2026-05-26 09:21:05 +0900

    pack.lua追加

---
+++ lua/util/pack.lua
@@ -0,0 +1,19 @@
+-- pack.lua
+-- use builtin package manager.
+
+local load_now = function(load) load() end
+local load_later = function(load)
+	vim.api.nvim_create_autocmd({ 'FileType' }, { callback = load })
+end
+
+local M = {}
+
+M.load = function(setup, do_it_later)
+	if (do_it_later) then
+		load_later(setup)
+	else
+		load_now(setup)
+	end
+end
+
+return M
