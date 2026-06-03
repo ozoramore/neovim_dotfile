72c88804a713c344708b88c7dd69f7dbada79c38
ozoramore <dev@t2arc.net>
2026-06-03 11:27:03 +0900

    パッケージ削除の処理を追加

--- lua/util/pack.lua
+++ lua/util/pack.lua
@@ -9,10 +9,22 @@ end
 local M = {}
 
 M.load = function(setup, do_it_later)
-	if (do_it_later) then
+	if do_it_later then
 		load_later(setup)
 	else
 		load_now(setup)
+	end
+end
+
+M.clean = function()
+	local packs = {}
+	for _, p in ipairs(vim.pack.get()) do
+		if not p.active then
+			table.insert(packs, p.spec.name)
+		end
+	end
+	if #packs > 0 then
+		vim.pack.del(packs)
 	end
 end
 
