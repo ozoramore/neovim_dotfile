17327dbae55a5bebef7c013bcd8d1211e6575889
ozoramore <dev@t2arc.net>
2026-06-09 08:57:17 +0900

    popup画面の背景をdimmしない

--- lua/util/style.lua
+++ lua/util/style.lua
@@ -6,17 +6,19 @@ M.ns = {
 
 local update = function(_)
 	local curwin = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_set_hl_ns(curwin, M.ns.active)
+
+	if string.len(vim.api.nvim_win_get_config(curwin).relative) ~= 0 then return end -- for floating window
+
 	for _, win in ipairs(vim.api.nvim_list_wins()) do
-		local ns = M.ns.inactive
-		if win == curwin then ns = M.ns.active end
-		vim.api.nvim_win_set_hl_ns(win, ns)
+		if win ~= curwin then vim.api.nvim_win_set_hl_ns(win, M.ns.inactive) end
 	end
 end
 
 M.setup = function()
 	M.ns.active = vim.api.nvim_get_namespaces()['active_window'] or 0
 	M.ns.inactive = vim.api.nvim_get_namespaces()['inactive_window'] or 0
-	vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', 'ModeChanged' }, { callback = update })
+	vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, { callback = update })
 end
 
 return M
